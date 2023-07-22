0cadf88a9df9a8b4eafb5be0d2a0d6e7a0589a98
Gerald-Hu <90969856+Gerald-Hu@users.noreply.github.com>
2023-07-21 20:18:15 -0400

    Update config_default.js

--- config/config_default.js
+++ config/config_default.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: "mongodb+srv://admin-pgh:lyz&hxm@cluster0.mzegswu.mongodb.net/TestDB",
+    db: "mongodb+srv://admin-pgh:lyz&hxm@cluster0.mzegswu.mongodb.net/TestDB", // This is my free MongoDB cluster. Feel free to use or add your own cluster.
     secret: "77744b5f-6945-41a4-8664-e91832d04bd8", // This is used for JWT if needed.
 }
